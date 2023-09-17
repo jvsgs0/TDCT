01ad1b26b53182ef09a7c71e642287e7606f0f0b
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2023-09-17 20:58:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function next() {
+    window.location = "1°pagina.html"
+}
+function next1() {
+    window.location = "2°pagina.html"
+}
+function next2() {
+    window.location = "3°pagina.html"
+}
+function next3() {
+    window.location = "4°pagina.html"
+}
+function voltar() {
+    window.location = "index.html"
+}
